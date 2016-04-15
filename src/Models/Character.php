6942722c46c47029e5335cccb9a65a5f6e32d3d9
Basilius Sauter <basilius.sauter@gmail.com>
2016-04-15 07:40:11 +0200

    Fixed Test

--- src/Models/Character.php
+++ src/Models/Character.php
@@ -8,14 +8,42 @@ use Doctrine\ORM\Mapping\Entity;
  * Description of Character
  *
  * @Entity
- * @Table(name="Characters")
+ * @Table(name="characters")
  */
 class Character {
     /** @Id @Column(type="integer") @GeneratedValue */
     private $id;
-    /** @Column(type="string", length=255, unique=true); */
+    /** @Column(type="string", length=50, unique=true); */
     private $name;
+    /** @Column(type="text", unique=true); */
+    private $displayName;
+    /** @Column(type="integer", options={"default" = 10}) */
     private $health = 10;
+    /** @Column(type="integer", options={"default" = 10}) */
     private $maxhealth = 10;
     private $properties;
+    
+    /**
+     * Sets the character's name and generates the display name
+     * @param string $name The name to set
+     */
+    public function setName(string $name) {
+        $this->name = $name;
+        $this->generateDisplayName();
+    }
+    
+    /**
+     * Returns the character's name
+     * @return string The name
+     */
+    public function getName(): string {
+        return $this->name;
+    }
+    
+    /**
+     * Generates the display name which is a composition of title and name.
+     */
+    protected function generateDisplayName() {
+        $this->displayName = $this->name;
+    }
 }
