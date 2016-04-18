bd254f262e0dc5993267c8da1486378a922b85cc
Basilius Sauter <basilius.sauter@gmail.com>
2016-04-18 07:06:34 +0200

    Closes #8

---
+++ src/Exceptions/ParentAlreadySetException.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace LotGD\Core\Exceptions;
+
+/**
+ * Exception if a specific, required argument is missing
+ */
+class AttributeMissingException extends CoreException {
+    
+}
