457dadeff1e04f480524f8f82454d5bdeb7962aa
Basilius Sauter <basilius.sauter@gmail.com>
2016-04-18 07:32:28 +0200

    Adds Scene model

--- src/Exceptions/ParentAlreadySetException.php
+++ src/Exceptions/ParentAlreadySetException.php
@@ -6,6 +6,6 @@ namespace LotGD\Core\Exceptions;
 /**
  * Exception if a specific, required argument is missing
  */
-class AttributeMissingException extends CoreException {
+class ParentAlreadySetException extends CoreException {
     
 }
