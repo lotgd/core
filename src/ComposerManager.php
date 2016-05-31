40383849b965af7926a1d66386ace9b12f8bf71c
Austen McDonald <austen.mcdonald@gmail.com>
2016-05-30 20:02:49 -0700

    Oops, need this for ModuleManager::validate command.

--- src/ComposerManager.php
+++ src/ComposerManager.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace LotGD\Core;
 
+use Composer\Composer;
+
 class ComposerManager
 {
     private $g;
@@ -20,11 +22,28 @@ class ComposerManager
      * Returns a Composer instance to perform underlying operations on. Be careful.
      * @return Composer An instance of Composer.
      */
-    public function getComposer()
+    public function getComposer(): Composer
     {
         if ($this->composer === null) {
             $this->composer = \Composer\Factory::create(new \Composer\IO\NullIO());
         }
         return $this->composer;
+    }
+
+    /**
+     * Return a list of the configured packages which are LotGD modules (type = 'lotgd-module').
+     * @return array Array of \Composer\PackageInterface.
+     */
+    public function getModulePackages(): array
+    {
+        $result = array();
+        $packages = $this->getComposer()->getRepositoryManager()->getLocalRepository()->getPackages();
+        foreach ($packages as $p) {
+            if ($p->getType() === 'lotgd-module') {
+                array_push($result, $p);
+            }
+        }
+
+        return $result;
     }
 }
