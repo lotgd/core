ad49ab05f85f76a4547f26f14a233335d70708b6
Austen McDonald <austen.mcdonald@gmail.com>
2016-06-04 11:58:56 -0700

    Reorder use statements.

--- src/Console/Command/ModuleRegisterCommand.php
+++ src/Console/Command/ModuleRegisterCommand.php
@@ -3,15 +3,14 @@ declare(strict_types=1);
 
 namespace LotGD\Core\Console\Command;
 
-use LotGD\Core\Bootstrap;
-
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 
+use LotGD\Core\Bootstrap;
 use LotGD\Core\Exceptions\ClassNotFoundException;
 use LotGD\Core\Exceptions\ModuleAlreadyExistsException;
 
