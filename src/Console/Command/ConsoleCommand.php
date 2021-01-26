53d13084132dad0dc614d80dfcb7fb80b0e107ac
Vassyli <basilius.sauter@gmail.com>
2021-01-26 18:31:30 +0100

    Regression fix: Adds composer requirement for d11wtq/boris back in.

--- src/Console/Command/ConsoleCommand.php
+++ src/Console/Command/ConsoleCommand.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace LotGD\Core\Console\Command;
 
+use LotGD\Core\Game;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -27,7 +28,7 @@ class ConsoleCommand extends BaseCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        print "Daenerys console, the dragon prompt. lotgd/core " . \LotGD\Core\Game::getVersion() . ".\n";
+        print "Daenerys console, the dragon prompt. lotgd/core " . Game::getVersion() . ".\n";
         print "Enter some PHP, but be careful, this is live and attached to your currently configured setup:\n\n";
         print $this->game->getConfiguration();
 
